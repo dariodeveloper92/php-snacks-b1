373b07c896415306af035180bb02e2c328ef02af
Dario <dario.defrancisci92@gmail.com>
2021-10-08 14:37:50 +0200

    creato scaffolding

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Snacks blocco 1</title>
+</head>
+<body>
+    
+    <h1> Snack 1</h1>
+    <h4>Creiamo un array contenente le partite di basket di un’ipotetica tappa del calendario:</h4>
+    <!-- Risultato: Olimpia Milano - Cantù | 55-60 -->
+
+    <h1> Snack 2</h1>
+    <h4>Passare come parametri GET name, mail e age: </h4>
+</body>
+</html>
